5ad8542af2218a95a47a0152ee15183bba04946b
Bachtiar Herdianto <69089212+bachtiarherdianto@users.noreply.github.com>
2023-08-17 07:08:22 +0200

    Update

--- Introduction-to-Keras/main.py
+++ Introduction-to-Keras/main.py
@@ -28,7 +28,7 @@ loss_val, accuracy_val = model.evaluate(X, y)
 print("Accuracy: %.2f" % (accuracy_val*100))
 
 """
-D:\HGS-PILS\euro-meets-neurips-2022-main\main>python main.py
+D:\python main.py
 
 2023-08-17 08:17:55.198474: I tensorflow/core/platform/cpu_feature_guard.cc:182] This TensorFlow binary is optimized to use available CPU instructions in performance-critical operations.
 To enable the following instructions: SSE SSE2 SSE3 SSE4.1 SSE4.2 AVX AVX2 FMA, in other operations, rebuild TensorFlow with the appropriate compiler flags.
